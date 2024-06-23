dc9aa7c80e88500bae811008c285d72d2f1cf386
Sainan <sainan@calamity.gg>
2024-06-23 04:00:12 +0000

    Deploying to gh-pages from @ calamity-inc/omni.wf@e5c971304719abd4f364e2ec715f0a1883319d0f 🚀

--- common.js
+++ common.js
@@ -1,1 +1,33 @@
-function getDictPromise(){return fetch("https://browse.wf/warframe-public-export-plus/dict."+(localStorage.getItem("lang")??"en")+".json").then(res=>res.json())}function setLanguage(code){setLanguageIndicator(code);fetch("https://browse.wf/warframe-public-export-plus/dict."+code+".json").then(res=>res.json()).then(function(dict){window.dict=dict;if("onLanguageUpdate"in window){onLanguageUpdate()}});localStorage.setItem("lang",code)}function setLanguageIndicator(code){document.querySelectorAll(".dropdown-item[data-lang]").forEach(elm=>elm.classList.remove("active"));const item=document.querySelector(".dropdown-item[data-lang="+code+"]");item.classList.add("active");document.getElementById("lang-dropdown").textContent=item.textContent}if(localStorage.getItem("lang")){setLanguageIndicator(localStorage.getItem("lang"))}
+function getDictPromise()
+{
+	return fetch("https://browse.wf/warframe-public-export-plus/dict." + (localStorage.getItem("lang") ?? "en") + ".json").then(res => res.json());
+}
+
+function setLanguage(code)
+{
+	setLanguageIndicator(code);
+
+	fetch("https://browse.wf/warframe-public-export-plus/dict." + code + ".json").then(res => res.json()).then(function(dict)
+	{
+		window.dict = dict;
+		if ("onLanguageUpdate" in window)
+		{
+			onLanguageUpdate();
+		}
+	});
+
+	localStorage.setItem("lang", code);
+}
+
+function setLanguageIndicator(code)
+{
+	document.querySelectorAll(".dropdown-item[data-lang]").forEach(elm => elm.classList.remove("active"));
+	const item = document.querySelector(".dropdown-item[data-lang=" + code + "]");
+	item.classList.add("active");
+	document.getElementById("lang-dropdown").textContent = item.textContent;
+}
+
+if (localStorage.getItem("lang"))
+{
+	setLanguageIndicator(localStorage.getItem("lang"));
+}
